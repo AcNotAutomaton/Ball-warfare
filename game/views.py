b9e7085061dc1bb0e0d5b51a1909c7b599319ce4
AcNotAutomaton <550578884@qq.com>
2023-05-11 22:21:16 +0800

    write my first project

--- game/views.py
+++ game/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">球球大作战</h1>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://cdn.acwing.com/media/file_system/file/application/icon/background_bHjjwub.gif" ' \
+            'width=500>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line4 = '<a href="/">返回主界面</a>'
+    line3 = '<hr>'
+    line2 = '<img src="https://cdn.acwing.com/media/file_system/file/application/icon/background_bHjjwub.gif" ' \
+            'width=500>'
+    return HttpResponse(line1 + line4 + line3 + line2)
