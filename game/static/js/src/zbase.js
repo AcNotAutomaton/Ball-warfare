c90f7e2ca05c66d0507820eb8c6d2f96e4fe0d52
AcNotAutomaton <550578884@qq.com>
2023-07-04 20:43:46 +0800

    版本换成3.2.19 清楚网页缓存

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,8 @@
-export class AcGame {
+class AcGame {
     constructor(id) {
         this.id = id;
         this.$ac_game = $('#' + id);
-        // this.menu = new AcGameMenu(this);
+        this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
 
         this.start();
