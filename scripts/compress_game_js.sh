fb214ad508bd4cd1d3c0afab9227a104b0ed2f13
wa_automata <550578884@qq.com>
2025-12-12 18:46:18 +0800

    创建build脚本，并且删除老脚本

--- scripts/compress_game_js.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-JS_PATH=../game/static/js/
-JS_PATH_DIST=${JS_PATH}dist/
-JS_PATH_SRC=${JS_PATH}src/
-
-find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
-
-#echo yes | python.exe manage.py collectstatic
